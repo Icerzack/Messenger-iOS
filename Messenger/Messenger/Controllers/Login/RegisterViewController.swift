3d1ee5e1af153aeb5c74e3935cbcfe4c96f921c0
Icerzack <maxalkuz@gmail.com>
2022-12-08 15:35:42 +0300

    add base and login screen

---
+++ Messenger/Messenger/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by Max Kuznetsov on 03.12.2022.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .link
+        
+    }
+
+}
